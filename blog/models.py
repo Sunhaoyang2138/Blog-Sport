40128c5a41675e3ace8b03fad7d209a7f58f17cb
sunhaoyang <sunhaoyang2138@gmail.com>
2019-06-26 11:46:38 +0200

    made changes

--- blog/models.py
+++ blog/models.py
@@ -8,10 +8,10 @@ class Post(models.Model):
     title=models.CharField(max_length=200)
     text=models.TextField()
     created_date=models.DateTimeField(default=timezone.now)
-    pulished_date=models.DateTimeField(blank=True, null=True)
+    published_date=models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.pulished_date=timezone.now()
+        self.published_date=timezone.now()
         self.save()
 
     def __str__(self):
