b87f1e39359145afa5af35670cb376202a8937fc
sunhaoyang <sunhaoyang2138@gmail.com>
2019-06-28 19:06:53 +0200

    made changes

--- blog/views.py
+++ blog/views.py
@@ -25,7 +25,7 @@ def post_new (request):
             post.save()
             return redirect('post_detail', pk=post.pk)
 
-   else:
+    else:
       form = PostForm()
       return render(request, 'blog/post_edit.html', {'form': form})
 
