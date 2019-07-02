c4a2873d90c30b0128952d93d1cf5fad22e14bb6
sunhaoyang <sunhaoyang2138@gmail.com>
2019-07-02 22:50:56 +0200

    made changes

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,13 @@ from django.utils import timezone
 class Post(models.Model):
     author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     title=models.CharField(max_length=200)
-    text=models.TextField()
+    text=models.TextField(blank=True, null=True)
     created_date=models.DateTimeField(default=timezone.now)
     published_date=models.DateTimeField(blank=True, null=True)
+    image=models.ImageField(upload_to='sports',blank=True,null=True)
+    location=models.CharField(max_length=200,blank=True,null=True)
+    sport=models.CharField(max_length=200,blank=True,null=True)
+    duration=models.DurationField(blank=True,null=True)
 
     def publish(self):
         self.published_date=timezone.now()
