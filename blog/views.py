dbb38641b5821c04aef2b31a2704401998c6fe26
sunhaoyang <sunhaoyang2138@gmail.com>
2019-07-03 19:06:43 +0200

    made changes

--- blog/views.py
+++ blog/views.py
@@ -26,8 +26,8 @@ def post_new (request):
             return redirect('post_detail', pk=post.pk)
 
     else:
-      form = PostForm()
-      return render(request, 'blog/post_edit.html', {'form': form})
+        form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
