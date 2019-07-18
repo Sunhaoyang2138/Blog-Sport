38d02fdaf55bd6b550e29d2e00bd50ea78a68390
Yujie Xie <xyj960320@hotmail.com>
2019-07-18 15:04:25 +0200

    made changes

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ from django.core.files.storage import FileSystemStorage
 # Create your views here.
 
 def post_list (request):
-   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
    return render(request, 'blog/post_list.html', {'posts' : posts})
 
 def post_detail(request, pk):
